0d2dc8b478268dce43fe922b9cb501f15ffed128
Gerard Gargan <gerardgargan@hotmail.co.uk>
2023-11-26 19:35:56 +0000

    added code to create server and listen for requests on port 3000

--- app.js
+++ app.js
@@ -1,1 +1,17 @@
-console.log("Hello Node World!");
+const http = require("http");
+const PORT = 3000;
+
+const server = http.createServer((req,res) => {
+    console.log("request starting...");
+    res.setHeader("Content-Type", "text/html");
+    res.write("Hello from Node JS Web Server!");
+    res.end();
+});
+
+server.listen(PORT, (err) => {
+    if(err){
+        return console.log(err);
+    }
+
+    console.log("Node web server listening on post http://localhost:"+PORT);
+})
